37d3faa04c841b19cb49b25033c8e5109454a37d
Ivan Dragiev <dragiev90@gmail.com>
2023-08-02 10:34:53 +0300

    little changes

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -1,10 +1,13 @@
 function start(state, game) {
+    game.createWizard(state.wizard);
+
+
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
 
 
 function gameLoop(state, game) {
-    console.log(state.player);
-    
+
+
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
--- src/js/gameObjects.js
+++ src/js/gameObjects.js
@@ -1,9 +1,24 @@
-function initGameObject(){
+function initGameObject() {
     const startScreen = document.querySelector('.start-screen');
     const gameScreen = document.querySelector('.game-screen');
-    
-    return{
+
+    return {
         startScreen,
-        gameScreen
+        gameScreen,
+        createWizard(initialState) {
+            let wizardElement = document.createElement('div');
+            wizardElement.classList.add('wizard');
+
+            wizardElement.style.height = initialState.width + 'px';
+            wizardElement.style.height = initialState.height + 'px';
+
+            wizardElement.style.left = initialState.startX + 'px';
+            wizardElement.style.left = initialState.startY + 'px';
+
+            this.wizardElement = wizardElement;
+            gameScreen.appendChild(wizardElement);
+
+            return wizard;
+        },
     }
 }
